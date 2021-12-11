907c34749fc9aa08e8365e33109e4981f0b08661
JohnWu <salancelot1014@gmail.com>
2021-12-11 17:10:01 +0800

    Add guess_game.rs and ownership_string.rs into the main.rs by "mod" keyword

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
+mod guess_game;
+mod ownership_string;
+
 use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    ownership_string
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    loop {
-        println!("Please input your guess:");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("Your guess: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
 }
